3cbab80ee0464f30b606da8bc61761ceeb4d61b8
neoa <acrixf@gmail.com>
2017-07-09 21:10:05 -0500

    Done

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -1,3 +1,33 @@
 /**
  * Created by neoa on 09/07/17.
  */
+export class Article {
+
+  public title : string;
+  public link : string;
+  public votes : number;
+
+  constructor(title: string, link: string, votes?: number) {
+    this.title = title;
+    this.link = link;
+    this.votes = votes;
+  }
+
+  public voteUp(): void {
+    this.votes +=1;
+  }
+
+  public voteDown(): void{
+    this.votes -=1;
+  }
+
+  public domain(): string{
+    try {
+      const link : string = this.link.split('//')[1];
+      console.log(link.split('/')[0]);
+      return link.split('/')[0];
+    }catch ( err ){
+      return null;
+    }
+  }
+}
